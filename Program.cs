66f7609c9ef46730b80fcb4aa1e553e0726883eb
Pravesh Mishra <pravesh.mishra@wonderbiz.in>
2025-11-03 12:31:04 +0530

    explore reflection basics

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Get the type of the Student class
+            Type type = typeof(Student);
+
+            Console.WriteLine("Class Name: " + type.Name);
+            Console.WriteLine("\n--- Properties ---");
+            foreach (PropertyInfo prop in type.GetProperties())
+            {
+                Console.WriteLine(prop.Name);
+            }
+
+            Console.WriteLine("\n--- Methods ---");
+            foreach (MethodInfo method in type.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+
+            // Create object using reflection
+            object obj = Activator.CreateInstance(type);
+            type.GetProperty("Id")?.SetValue(obj, 1);
+            type.GetProperty("Name")?.SetValue(obj, "Pravesh");
+
+            // Call method using reflection
+            MethodInfo displayMethod = type.GetMethod("DisplayInfo");
+            displayMethod.Invoke(obj, null);
+        }
+    }
+}
+
---
+++ Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}");
+        }
+    }
+}
